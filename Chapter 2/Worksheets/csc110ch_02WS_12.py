e13d31f223ad80a732a0b59ab519f0f42c00278d
JonJT <jontally@gmail.com>
2020-04-17 18:20:24 -0700

    update

--- Chapter 2/Worksheets/csc110ch_02WS_12.py
+++ Chapter 2/Worksheets/csc110ch_02WS_12.py
@@ -3,5 +3,9 @@
 # Use a for loop with range to sum all the  odd numbers from 11 to 91.  Use a sum accumulator.
 # **************************************
 
-for i in range(11,99,2):
-    
+start = 11
+end = 91
+
+num_list = range(start, end + 1, 2)
+final_sum = sum(num_list)
+print(final_sum)
