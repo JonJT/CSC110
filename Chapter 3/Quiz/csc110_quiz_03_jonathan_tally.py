5b4dc82a9660568809242a54014391d8a526f931
JonJT <jontally@gmail.com>
2020-05-02 17:09:16 -0700

    update

--- Chapter 3/Quiz/csc110_quiz_03_jonathan_tally.py
+++ Chapter 3/Quiz/csc110_quiz_03_jonathan_tally.py
@@ -1,4 +1,5 @@
 # CSC 110 Spring 2020  Quiz 3 Take-home                       NAME: Jonathan Tally
+# Youtube Link: https://youtu.be/qiEEMgE1EvQ
 # Write clear, legible code, the simpler the better. Make sure your code works.   Due Monday at class start.  
 # Use the editor to write your code.  Also, submit a file, Quiz_3_YourFullName.py in an email that has as subject csc 110 quiz 3_YourFullName.   
 # Points will be deducted if submission is late.
@@ -9,24 +10,24 @@
 
 # mltyClrdSqr(tess,150,'red’, 'green’, 'blue’,  'magenta')
 
-import turtle
+import turtle               # import turtle
 
-wn = turtle.Screen()
-t = turtle.Turtle()
-wn.bgcolor("black")
-t.pensize(5)
-t.speed(0)
-
-
-def mltyClrdSqr(t, sz, clr1, clr2, clr3, clr4):
-    '''turtle, t, assign color clr#, draw square'''
-    for i in (clr1, clr2, clr3, clr4):
-        t.color(i)
-        t.fd(sz)
-        t.lt(90)
+wn = turtle.Screen()        # assign turtle screen to wn
+t = turtle.Turtle()         # assign turtle to t
+wn.bgcolor("black")         # assign background color black
+t.pensize(5)                # adjust pen size
+t.speed(0)                  # adjust draw speed to 0 "Fastest"
 
 
-mltyClrdSqr(t, 50, 'red', 'green', 'blue', 'magenta')
+def mltyClrdSqr(t, sz, clr1, clr2, clr3, clr4):                                 # Create function definition name ( Parameters )
+    '''turtle, t, assign color, clr#, draw square with side length of, sz'''
+    for i in (clr1, clr2, clr3, clr4):                                          # for loop with 4 colors creating a square
+        t.color(i)                                                              # each loop with start with a different color from the sequence
+        t.fd(sz)                                                                # set the size of each line from the function argument
+        t.lt(90)                                                                # set the turn in degrees
+
+
+mltyClrdSqr(t, 50, 'red', 'green', 'blue', 'magenta')                           # calling function and setting arguments
 
 # 2.  (10 pts)  Use the previous function as a helper function to write the function
 # lineOfMltyClrdSqrs(t, sz, , clr1, clr2, clr3, clr4, spc, n), where turtle, t,  draws 
